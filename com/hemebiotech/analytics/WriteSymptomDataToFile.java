c6f22825033bfd3410d3dbddcd073ad1ac573f75
Abdelhakim Zerargui <444k4rh1s1g9-student@anonymous-codio.com>
2023-05-19 19:14:27 +0000

    writer implementation

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -9,14 +9,23 @@ public class WriteSymptomDataToFile implements ISymptomWriter{
 
 @Override
 public void writeSymptoms(Map<String, Integer> symptoms){
+String filePath = "result.out";
 
     try{ 
-        FileWriter writer = new FileWriter ("result.out");
-        writer.close();
+	    FileWriter writer = new FileWriter (filePath);
+        Iterator<Map.Entry<String, Integer>> iterator = symptoms.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, Integer> entry = iterator.next();
+                String symptom = entry.getKey();
+                int count = entry.getValue();
+                String line = symptom + ": " + count + System.lineSeparator();
+                writer.write(line);
+            }
+        
     }   catch (IOException e) {
 				e.printStackTrace();
 			}
 }
 
 
-}
+ }
